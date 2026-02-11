a84852f0dc07c0e0b168ce6cd78eb5979b2bf9b6
ykhan1999 <yousefkhan125@gmail.com>
2026-02-11 12:58:47 -0500

    add captive portal support for offline mode

--- scripts/init/install_opennds.sh
+++ scripts/init/install_opennds.sh
@@ -7,3 +7,7 @@ make
 sudo make install
 sudo cp resources/opennds.conf.ExtendFi /etc/config/opennds
 sudo cp resources/theme_click-to-continue-basic.sh /usr/lib/opennds/theme_click-to-continue-basic.sh
+#add captive portal overrides
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+sudo mkdir -p /etc/NetworkManager/dnsmasq-shared.d
+sudo cp ${SCRIPT_DIR}/helpers/captive-portal.conf /etc/NetworkManager/dnsmasq-shared.d/
